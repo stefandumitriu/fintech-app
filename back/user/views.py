24da7135b6a599a28aea62639dbd406e3e3d73d5
stefandumitriu <dumitriustefan21@gmail.com>
2022-04-29 11:47:58 +0300

    Implemented basic login request

--- back/user/views.py
+++ back/user/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import CustomUser, Account
 from .serializers import CustomUserSerializer, AccountSerializer
+from .auth import authenticate
 
 # Create your views here.
 
@@ -38,3 +39,14 @@ def account_list(req):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.data, status=405)
+
+@csrf_exempt
+def login_request(req):
+    data = JSONParser().parse(req)
+    user_name = data['user_name']
+    password = data['password']
+    user = authenticate(username=user_name, password=password)
+    if user is not None:
+        return HttpResponse("Login successful", status=201)
+    else:
+        return HttpResponse("Login failed", status=400)
