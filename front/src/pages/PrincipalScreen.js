9f94cab44f69287bf6d2fab258b2938801460be5
gabrielpoalelungi <gabriel1.poalelungi@gmail.com>
2022-05-02 17:16:22 +0300

    configured TransactionPage for JSON

--- front/src/pages/PrincipalScreen.js
+++ front/src/pages/PrincipalScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, StatusBar, Image} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, StatusBar, Image, ActivityIndicator} from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import OcticonsIcons from 'react-native-vector-icons/Octicons';
 import FontistoIcons from 'react-native-vector-icons/Fontisto';
@@ -17,191 +17,212 @@ export default class PrincipalScreen extends React.Component {
         }
     }
 
+    componentDidMount() {
+
+        return fetch('http://10.0.2.2:3000/current_account')
+            .then( (response) => response.json())
+            .then( (responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                })
+            })
+            .catch((error) => {console.log(error)});
+    }
+
     render() {
-        let flag = '';
-        let currency = '';
-        if (testJsonFile.currency === 'lei') {
-            flag = 'romania';
-            currency = 'RON';
-        }
-        else if (testJsonFile.currency === 'euro') {
-            flag = "european-union";
-            currency = '\u20AC';
-        }
-        return (
-            <>
-                <StatusBar backgroundColor='#7de24e'></StatusBar>
-                <View style={styles.container}>
-                    <TouchableOpacity
-                        style={styles.signOutButton}
-                        activeOpacity={0.5}
-                        onPress={() => this.props.navigation.replace('Auth')}>
-                        <FontAwesomeIcon name="home" size={30} style={{left: 4, top: 2}}></FontAwesomeIcon>
-                    </TouchableOpacity>
-                    <View>
-                        <Text style={styles.dashboardText}>Dashboard</Text> 
+        if (this.state.isLoading) {
+            return (
+                <View>
+                    <ActivityIndicator/>
+                </View>
+            )
+        } else {
+            let flag = '';
+            let currency = '';
+            if (this.state.dataSource[0].currency === 'lei') {
+                flag = 'romania';
+                currency = 'RON';
+            }
+            else if (this.state.dataSource[0].currency === 'euro') {
+                flag = "european-union";
+                currency = '\u20AC';
+            }
+            return (
+                <>
+                    <StatusBar backgroundColor='#7de24e'></StatusBar>
+                    <View style={styles.container}>
+                        <TouchableOpacity
+                            style={styles.signOutButton}
+                            activeOpacity={0.5}
+                            onPress={() => this.props.navigation.replace('Auth')}>
+                            <FontAwesomeIcon name="home" size={30} style={{left: 4, top: 2}}></FontAwesomeIcon>
+                        </TouchableOpacity>
+                        <View>
+                            <Text style={styles.dashboardText}>Dashboard</Text> 
 
-                        {/* Current account and amount of money */}
-                        <View style = {styles.borderAccounts}>
+                            {/* Current account and amount of money */}
+                            <View style = {styles.borderAccounts}>
 
-                            <View style={styles.flagStyle}>
-                                <FlagIcon name={flag} height="60" width="60"></FlagIcon>
-                            </View>
+                                <View style={styles.flagStyle}>
+                                    <FlagIcon name={flag} height="60" width="60"></FlagIcon>
+                                </View>
 
-                            <Text style = {styles.amountText}>
-                                {currency}69420.69
-                            </Text>
-
-                        </View> 
-
-                        {/* Recent transactions. Press the white slide*/}
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('TransactionPage')}>
-                            <View style = {styles.recentTransactions}>
-                                <Text style = {styles.recentTransactionTitle}>
-                                        Recent Transactions
+                                <Text style = {styles.amountText}>
+                                    {currency}{this.state.dataSource[0].cash}
                                 </Text>
 
-                                <View >
-                                    <Image style = {{
-                                                top: "30%",
-                                                left: "3%",
-                                                borderRadius: 20
-                                            }} 
-                                            source={{width:35, height:35, uri: 'https://picsum.photos/200'}}
-                                    ></Image>
-                                    <Image style = {{
-                                                top: "60%",
-                                                left: "3%",
-                                                borderRadius: 20
-                                            }} 
-                                            source={{width:35, height:35, uri: 'https://picsum.photos/200'}}></Image>
-                                </View>
+                            </View> 
 
-                                <View >
-                                    <Text 
-                                        style = {{
-                                            fontSize: 15,
-                                            fontWeight: "bold",
-                                            alignSelf: 'center',
-                                            top: "-100%",
-                                            left: "-5%"
-                                        }}
-                                    >
-                                            To: GabrielP
+                            {/* Recent transactions. Press the white slide*/}
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('TransactionPage')}>
+                                <View style = {styles.recentTransactions}>
+                                    <Text style = {styles.recentTransactionTitle}>
+                                            Recent Transactions
                                     </Text>
-                                    <Text 
-                                        style = {{
-                                            fontSize: 15,
-                                            fontWeight: "bold",
-                                            alignSelf: 'center',
-                                            top: "-18%",
-                                            left: "-5%"
-                                        }}
-                                    >
-                                            From: GabrielI
-                                    </Text>
-                                </View>
 
+                                    <View >
+                                        <Image style = {{
+                                                    top: "30%",
+                                                    left: "3%",
+                                                    borderRadius: 20
+                                                }} 
+                                                source={{width:35, height:35, uri: 'https://picsum.photos/200'}}
+                                        ></Image>
+                                        <Image style = {{
+                                                    top: "60%",
+                                                    left: "3%",
+                                                    borderRadius: 20
+                                                }} 
+                                                source={{width:35, height:35, uri: 'https://picsum.photos/200'}}></Image>
+                                    </View>
+
+                                    <View >
+                                        <Text 
+                                            style = {{
+                                                fontSize: 15,
+                                                fontWeight: "bold",
+                                                alignSelf: 'center',
+                                                top: "-100%",
+                                                left: "-5%"
+                                            }}
+                                        >
+                                                To: GabrielP
+                                        </Text>
+                                        <Text 
+                                            style = {{
+                                                fontSize: 15,
+                                                fontWeight: "bold",
+                                                alignSelf: 'center',
+                                                top: "-18%",
+                                                left: "-5%"
+                                            }}
+                                        >
+                                                From: GabrielI
+                                        </Text>
+                                    </View>
+
+                                    <View>
+                                        <Text 
+                                            style = {{
+                                                fontSize: 15,
+                                                fontWeight: "bold",
+                                                alignSelf: 'center',
+                                                top: "-203%",
+                                                left: "35%"
+                                            }}
+                                        >
+                                                RON 6.9
+                                        </Text>
+
+                                        <Text 
+                                            style = {{
+                                                fontSize: 15,
+                                                fontWeight: "bold",
+                                                alignSelf: 'center',
+                                                top: "-120%",
+                                                left: "35%"
+                                            }}
+                                        >
+                                                RON 6.9
+                                        </Text>
+                                    </View>
+                                    
+                                </View> 
+                            </TouchableOpacity>
+                            
+
+                            {/* Overall balance */}
+                            <View style = {styles.overallBalance}>
                                 <View>
-                                    <Text 
-                                        style = {{
-                                            fontSize: 15,
-                                            fontWeight: "bold",
-                                            alignSelf: 'center',
-                                            top: "-203%",
-                                            left: "35%"
-                                        }}
-                                    >
-                                            RON 6.9
-                                    </Text>
-
-                                    <Text 
-                                        style = {{
-                                            fontSize: 15,
-                                            fontWeight: "bold",
-                                            alignSelf: 'center',
-                                            top: "-120%",
-                                            left: "35%"
-                                        }}
-                                    >
-                                            RON 6.9
+                                    <Text style = {styles.overallBalanceTitle}>
+                                            Overall Balance
                                     </Text>
                                 </View>
                                 
-                            </View> 
-                        </TouchableOpacity>
-                        
-
-                        {/* Overall balance */}
-                        <View style = {styles.overallBalance}>
-                            <View>
-                                <Text style = {styles.overallBalanceTitle}>
-                                        Overall Balance
-                                </Text>
-                            </View>
-                            
-                            <View style={{top: "10%"}}>
-                                <Text style = {styles.overallBalanceCash}>
-                                        Cash
-                                </Text>
-                                <Text style = {styles.overallBalanceStocks}>
-                                        Stocks
-                                </Text>
-                                <Text style = {styles.overallBalanceSafe}>
-                                        Safe
-                                </Text>
-                            </View>
-
-                            <View style={{top: "-28%", left: "10%"}}>
-                                <Text style = {styles.amountOverallBalanceCash}>
-                                    RON 200.04
+                                <View style={{top: "10%"}}>
+                                    <Text style = {styles.overallBalanceCash}>
+                                            Cash
                                     </Text>
-                                <Text style = {styles.amountOverallBalanceStocks}>
-                                        RON 1.04
+                                    <Text style = {styles.overallBalanceStocks}>
+                                            Stocks
                                     </Text>
-                                <Text style = {styles.amountOverallBalanceSafe}>
-                                    RON 200000.04
-                                </Text>
+                                    <Text style = {styles.overallBalanceSafe}>
+                                            Safe
+                                    </Text>
+                                </View>
+
+                                <View style={{top: "-28%", left: "10%"}}>
+                                    <Text style = {styles.amountOverallBalanceCash}>
+                                        {currency}{this.state.dataSource[0].cash}
+                                        </Text>
+                                    <Text style = {styles.amountOverallBalanceStocks}>
+                                            {currency}{this.state.dataSource[0].stocks}
+                                        </Text>
+                                    <Text style = {styles.amountOverallBalanceSafe}>
+                                        {currency}{this.state.dataSource[0].safe}
+                                    </Text>
+                                </View>
                             </View>
                         </View>
                     </View>
-                </View>
 
-                {/* Bottom menu bar */}
-                <View style={styles.menuBar}>
-                    <View style={styles.homeButton}>
-                            <TouchableOpacity onPress={() => alert("Home button pressed!")}>
-                                <FontAwesomeIcon name="home" size={40} style={{left: 3, top: 2}}></FontAwesomeIcon>
+                    {/* Bottom menu bar */}
+                    <View style={styles.menuBar}>
+                        <View style={styles.homeButton}>
+                                <TouchableOpacity onPress={() => alert("Home button pressed!")}>
+                                    <FontAwesomeIcon name="home" size={40} style={{left: 3, top: 2}}></FontAwesomeIcon>
+                                </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.stocksButton}>
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Stocks')}>
+                                <FeatherIcons name="trending-up" size={40} style={{left: 5, top: 3}}></FeatherIcons>
                             </TouchableOpacity>
-                    </View>
+                        </View>
 
-                    <View style={styles.stocksButton}>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Stocks')}>
-                            <FeatherIcons name="trending-up" size={40} style={{left: 5, top: 3}}></FeatherIcons>
-                        </TouchableOpacity>
-                    </View>
+                        <View style={styles.transferButton}>
+                            <TouchableOpacity onPress={() => alert("Transfer button pressed!")}>
+                                <FontistoIcons name="arrow-swap" size={35} style={{left: 5, top: 5}}></FontistoIcons>
+                            </TouchableOpacity>
+                        </View>
 
-                    <View style={styles.transferButton}>
-                        <TouchableOpacity onPress={() => alert("Transfer button pressed!")}>
-                            <FontistoIcons name="arrow-swap" size={35} style={{left: 5, top: 5}}></FontistoIcons>
-                        </TouchableOpacity>
-                    </View>
+                        <View style={styles.walletButton}>
+                            <TouchableOpacity onPress={() => alert("Wallet button pressed!")}>
+                                <FontistoIcons name="wallet" size={35} style={{left: 5, top: 5}}></FontistoIcons>
+                            </TouchableOpacity>
+                        </View>
 
-                    <View style={styles.walletButton}>
-                        <TouchableOpacity onPress={() => alert("Wallet button pressed!")}>
-                            <FontistoIcons name="wallet" size={35} style={{left: 5, top: 5}}></FontistoIcons>
-                        </TouchableOpacity>
+                        <View style={styles.spendingAnalysisButton}>
+                            <TouchableOpacity onPress={() => alert("SpendingAnalysis button pressed!")}>
+                                <OcticonsIcons name="graph" size={40} style={{left: 5, top: 2}}></OcticonsIcons>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-
-                    <View style={styles.spendingAnalysisButton}>
-                        <TouchableOpacity onPress={() => alert("SpendingAnalysis button pressed!")}>
-                            <OcticonsIcons name="graph" size={40} style={{left: 5, top: 2}}></OcticonsIcons>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </>
-            
-        );
+                </>
+                
+            );
+        }
     }
 }
 
--- front/src/pages/TransactionPage.js
+++ front/src/pages/TransactionPage.js
@@ -1,34 +1,103 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ActivityIndicator, Text } from 'react-native';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
 
 // Tested to print transactions depending on an array
 export default class TransactionPage extends React.Component {
-    state = {
-        counters: [1, 2, 3, 4, 5, 6]
-      };
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            dataSource: null,
+            counters: [1, 2, 3]
+        }
+    }
+
+    componentDidMount() {
+
+        return fetch('http://10.0.2.2:3000/recent_transactions')
+            .then( (response) => response.json())
+            .then( (responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                })
+            })
+            .catch((error) => {console.log(error)});
+    }
     list() {
-        return this.state.counters.map( (number) => {
-                return (
-                    <View>
-                        <AntDesignIcons name="arrowdown" size={40} style={{left: 4, top: 2}} color='#7de24e'></AntDesignIcons>
-                        <AntDesignIcons name="arrowup" size={40} style={{left: 4, top: 2}} color='#7de24e'></AntDesignIcons>
-                    </View>
-                );
+        return this.state.dataSource.map( (recentTransaction) => {
+                var currency = '';
+                if (recentTransaction.currency === 'RON')
+                    currency = "RON";
+                else if (recentTransaction.currency === 'EUR')
+                    currency = '\u20AC';
+
+                if (recentTransaction.this_sent === true) {
+                    return (
+                        <View key={recentTransaction.id}>
+                            <AntDesignIcons name="arrowup" size={40} style={styles.arrows} color='#7de24e' />
+                            <Text style={styles.userText}>
+                                To: {recentTransaction.who}
+                            </Text>
+                            <Text style={styles.amount}>
+                                - {currency} {recentTransaction.amount}
+                            </Text>
+                        </View>
+                    );
+                } else {
+                    return (
+                        <View key={recentTransaction.id}>
+                            <AntDesignIcons name="arrowdown" size={40} style={styles.arrows} color='#7de24e' />
+                            <Text style={styles.userText}>
+                                From: {recentTransaction.who}
+                            </Text>
+                            <Text style={styles.amount}>
+                                {currency} {recentTransaction.amount}
+                            </Text>
+                        </View>
+                    );
+                }
             }
         );
     }
 
     render() {
-        return (
-            <View>
-                {this.list()}
-            </View>
-        );
+        if (this.state.isLoading) {
+            return (
+                <View>
+                    <ActivityIndicator/>
+                </View>
+            )
+        } else {
+            return (
+                <View>
+                    {this.list()}
+                </View>
+                
+            );
+        }
     }
 }
 
 const styles = StyleSheet.create({
-    
+    arrows: {
+        left: "2%",
+        top: "30%"
+    },
+    userText: {
+        fontSize: 15,
+        fontWeight: "bold",
+        alignSelf: 'center',
+        top: "-2%",
+        left: "-5%"
+    },
+    amount: {
+        fontSize: 15,
+        fontWeight: "bold",
+        alignSelf: 'center',
+        top: "-26%",
+        left: "35%"
+    }
 })
