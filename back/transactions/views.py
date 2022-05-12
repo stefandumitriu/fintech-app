6f3cc15b713fad4768616f8acc0368a2367fa038
stefandumitriu <dumitriustefan21@gmail.com>
2022-05-12 14:41:22 +0300

    Linked transaction amount to account balance

--- back/transactions/views.py
+++ back/transactions/views.py
@@ -1,6 +1,8 @@
-from django.db.models import Q
-from django.shortcuts import render
+import decimal
+import json
 
+import requests
+from django.db.models import Q, F
 # Create your views here.
 from rest_framework import authentication, status
 from rest_framework.parsers import JSONParser
@@ -10,6 +12,7 @@ from rest_framework.views import APIView
 
 from .models import Transaction
 from .serializer import TransactionSerializer
+from user.models import Account
 
 
 class TransactionView(APIView):
@@ -32,6 +35,39 @@ class TransactionView(APIView):
         serializer = TransactionSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
+            sender_accounts = Account.objects.filter(owner__email__contains=serializer.data['sender'])
+            account_with_needed_currency = sender_accounts.filter(currency__contains=serializer.data['currency'])
+            if account_with_needed_currency.exists():
+                Account.objects.filter(id=account_with_needed_currency[0].id).update(balance=F('balance') - serializer.data['amount'])
+            else:
+                acc = Account.objects.get(id=sender_accounts[0].id)
+                acc.balance -= exchange(serializer.data['currency'], acc.currency, serializer.data['amount'])
+                acc.save()
+            receiver_accounts = Account.objects.filter(owner__email__contains=serializer.data['receiver'])
+            account_with_needed_currency = receiver_accounts.filter(currency__contains=serializer.data['currency'])
+            if account_with_needed_currency.exists():
+                Account.objects.filter(id=account_with_needed_currency[0].id).update(
+                    balance=F('balance') + serializer.data['amount'])
+            else:
+                acc = Account.objects.get(id=receiver_accounts[0].id)
+                acc.balance += exchange(serializer.data['currency'], acc.currency, serializer.data['amount'])
+                acc.save()
             return Response(serializer.data)
         return Response({"error": serializer.errors,
                          "status": status.HTTP_203_NON_AUTHORITATIVE_INFORMATION})
+
+
+def exchange(from_currency, to_currency, amount):
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
+
+    payload = {}
+    headers = {
+        "apikey": "6LnKNclG2tnGHc4cFHsSArKaKL3YjxH6"
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        json_obj = json.loads(response.text)
+        return decimal.Decimal(json_obj['result'])
--- back/user/models.py
+++ back/user/models.py
@@ -48,8 +48,8 @@ class Account(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=20, decimal_places=2)
     currency = models.CharField(max_length=3)
-    creation_time = models.DateTimeField('creation date', auto_now_add=True)
-    last_updated_time = models.DateTimeField('last updated date', auto_now=True)
+    creation_time = models.DateTimeField(auto_now=True, null=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
 
     objects = models.Manager()
 
