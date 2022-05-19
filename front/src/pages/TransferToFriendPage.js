a2341348f55467c92e855b2f8af09fc4acf336a4
gabrielpoalelungi <gabriel1.poalelungi@gmail.com>
2022-05-19 13:58:49 +0300

    Finished 99% of "Transfer to Friend"

--- front/src/pages/TransferToFriendPage.js
+++ front/src/pages/TransferToFriendPage.js
@@ -1,19 +1,114 @@
 import axios from 'axios';
 import React, {Component} from 'react';
-import { TouchableOpacity, View, StyleSheet, Text, StatusBar, Image } from 'react-native';
+import { View, StyleSheet, Text, StatusBar, Button, TextInput, ActivityIndicator } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const baseUrl = 'http://10.0.2.2:8000';
 export default class TransferPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            currency: 'USD',
+            receiver: 'dummyText',
+            amount: '0',
+            token: null,
+            email: null,
+            isLoadingCredentials: true,
         }
       }
-    render() {
-        return (
-            <View>
-                <Text>MERGE!</Text>
-            </View>
-        );
+
+    
+    async getUser() {
+        try {
+            const savedUser = await AsyncStorage.getItem("loginInfo")
+            .then(async (res) => {
+                const val = await res;
+                const currentUser = JSON.parse(val);
+                this.setState({
+                    token: currentUser.token,
+                    email: currentUser.email,
+                    isLoadingCredentials: false,
+                });
+            });       
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    componentDidMount() {
+        this.getUser();
+    }
+
+      doTransfer() {
+        axios({
+            method: "POST",
+            url: "http://10.0.2.2:8000/transactions/",
+            data: {
+                amount: this.state.amount,
+                currency: this.state.currency,
+                sender: this.state.email,
+                receiver: this.state.receiver
+            },
+            headers: {Authorization: this.state.token, "Content-Type": "application/json"}
+          }).then(response => alert("Transfer Done!"))
+          .catch(error => console.log(error))
+      }
+
+      render() {
+        if (this.state.isLoadingCredentials == true) {
+            return (
+                <View>
+                    <ActivityIndicator/>
+                </View>
+            )
+        } else {
+            return (
+                <>
+                    <StatusBar backgroundColor='#7de24e'></StatusBar>
+                    <View style={styles.container}>
+                        <Text style={{fontSize: 35, fontWeight: "bold", alignSelf: 'center', top: "7%"}}>
+                                        Transfer to Friend
+                        </Text>
+                        <View style = {styles.borderAccounts}>
+                            <TextInput 
+                                style={styles.receiverText} 
+                                placeholder='Mail of receiver'
+                                onChangeText={newText => this.setState({receiver: newText})}
+                            />
+                        </View>
+
+                        <View style = {styles.recentTransactions}>
+                            <TextInput 
+                                style={styles.receiverText} 
+                                placeholder='Amount'
+                                onChangeText={newText => this.setState({amount: newText})}
+                            />
+                        </View>
+                        <View style={styles.currencyTile}>
+                            <Picker 
+                                style={{top: "-15%", left: "2%"}} 
+                                onValueChange={(itemValue) => this.setState({currency: itemValue})}
+                                selectedValue={this.state.currency}
+                            >
+                                <Picker.Item label="USD" value="USD" />
+                                <Picker.Item label="EUR" value="EUR" />
+                                <Picker.Item label="RON" value="RON" />
+                                <Picker.Item label="GBP" value="GBP" />
+                            </Picker>
+                        </View>
+
+                        <View style={{top: "40%"}}>
+                            <Button
+                            onPress={() => this.doTransfer()}
+                            title="Confirm Transfer"
+                            color="#7de24e"
+                            />
+                        </View>
+                    </View>
+                </>
+            );
+        }
     }
 }
 
@@ -24,30 +119,33 @@ const styles = StyleSheet.create(
             flex: 2,
             alignItems: "center",
         },
+        currencyTile: {
+            backgroundColor: 'white',
+            width: 350,
+            height: 40,
+            borderRadius: 20,
+            top: 150,
+        },
+        receiverText: {
+            fontSize: 20, 
+            fontWeight: "bold",
+            alignItems: "center",
+            top: "10%",
+            left: "2%"
+        },
         borderAccounts: {
             backgroundColor: 'white',
             width: 350,
-            height: 80,
+            height: 40,
             borderRadius: 20,
             top: 100,
         },
         recentTransactions: {
             backgroundColor: 'white',
             width: 350,
-            height: 80,
+            height: 40,
             borderRadius: 20,
             top: 125, 
-        },
-        signOutButton: {
-            position: 'absolute',
-            left: 10,
-            backgroundColor: '#7de24e',
-            color: '#ffffff',
-            borderColor: '#7de24e',
-            height: 35,
-            width: 35,
-            borderRadius: 30,
-            marginTop: 10,
-          },
+        }
     }
 );
